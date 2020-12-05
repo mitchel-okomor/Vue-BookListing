98a31331111024610e1d3559fcafb00b4b4d632b
CodeXaver <mitchel.okomor@gmail.com>
2020-12-05 14:28:02 +0100

    adding child component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li>{{book.title}}:{{book.author}}</li>
+</template>
+
+<script>
+export default {
+  name: "BookItem",
+  props: ['book']
+}
+</script>
+
+<style>
+h1,h2{
+  font-weight: normal;
+  }
+  ul{
+    list-style-type: none;
+    padding: 0;
+  }
+  li{
+    display: block;
+    margin: 0 10px;
+    }
+</style>
